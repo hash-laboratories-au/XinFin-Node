78cb3e5fd4790c44654f8808c21fc27f824993c0
Liam Lai <liam.icheng.lai@gmail.com>
2022-02-15 10:17:30 +0300

    update log file and docker compose

--- devnet/docker-up.sh
+++ devnet/docker-up.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-
-TIME=$(date +%s) HOSTIP=$(curl https://checkip.amazonaws.com) docker-compose -f docker-compose.yml up --build --force-recreate -d
+rm -f xdcchain-1/xdc.log.bak
+mv xdcchain-1/xdc.log xdcchain-1/xdc.log.bak
+rm -f xdcchain-2/xdc.log.bak
+mv xdcchain-2/xdc.log xdcchain-1/xdc.log.bak
+HOSTIP=$(curl https://checkip.amazonaws.com)  docker-compose -f docker-compose.yml up -d
