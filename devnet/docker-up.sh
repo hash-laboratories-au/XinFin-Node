fd11565e37cf0bbd346d51bfa97fc1bc912ff46c
Liam Lai <liam.icheng.lai@gmail.com>
2022-02-15 13:05:53 +0300

    add time parameter

--- devnet/docker-up.sh
+++ devnet/docker-up.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -f xdcchain-1/xdc.log.bak
-mv xdcchain-1/xdc.log xdcchain-1/xdc.log.bak
+mv xdcchain-1/xdc.log xdcchain-1/xdc.log.bak | true
 rm -f xdcchain-2/xdc.log.bak
-mv xdcchain-2/xdc.log xdcchain-1/xdc.log.bak
+mv xdcchain-2/xdc.log xdcchain-1/xdc.log.bak | true
 TIME=$(date +%s) HOSTIP=$(curl https://checkip.amazonaws.com)  docker-compose -f docker-compose.yml up -d
