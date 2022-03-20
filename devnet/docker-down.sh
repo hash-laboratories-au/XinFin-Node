eb2dc3a3bb50907b1f996798ea2aceff946f6565
Liam Lai <liam.icheng.lai@gmail.com>
2022-03-20 09:53:10 +0100

    improve devnet

--- devnet/docker-down.sh
+++ devnet/docker-down.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+WORKDIR=/root/XinFin/XinFin-Node/devnet
+cd $WORKDIR
+
 TIME=$(date +%s) HOSTIP=$(curl https://checkip.amazonaws.com) docker-compose -f docker-compose.yml down
--- devnet/pre-rollback.sh
+++ devnet/pre-rollback.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -x
 
+WORKDIR=/root/XinFin/XinFin-Node/devnet
+cd $WORKDIR
+
 rm -rf ~/6944661-1 ~/6944661-2
 
 cp -r ~/6944661 ~/6944661-1
